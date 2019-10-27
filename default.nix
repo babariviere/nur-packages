9e555dc89b366eea149e9603625ba9c8d2bedce6
Bastien Rivière <babathriviere@gmail.com>
2019-10-27 18:05:46 +0100

    remove lib as it can't be used properyly in configuration.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,5 @@
 { pkgs ? import <nixpkgs> { } }:
 
 {
-  lib = import ./lib { inherit pkgs; };
   nerd-font-symbols = pkgs.callPackage ./pkgs/nerd-font-symbols { };
 }
--- lib/default.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs }:
-
-{
-  dotfiles = import ./dotfiles { inherit pkgs; };
-}
